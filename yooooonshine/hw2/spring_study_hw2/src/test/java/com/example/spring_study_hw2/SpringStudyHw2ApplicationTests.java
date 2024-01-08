e1299be470e70a07dad7c481807f9452f92f9b70
yooooonshine <hl3470@naver.com>
2024-01-08 17:40:18 +0900

    Feat: hw2 과제 형식 제출

---
+++ yooooonshine/hw2/spring_study_hw2/src/test/java/com/example/spring_study_hw2/SpringStudyHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_study_hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
