e366b622621f798289bb94329b0708eaf028ddc4
seoyeon5117 <seoyun020207@gmail.com>
2024-01-16 13:05:34 +0900

    feat. hw3 제출

---
+++ seoyeon5117/hw3/src/main/java/homework/login/controller/MemberController.java
@@ -0,0 +1,45 @@
+package homework.login.controller;
+
+import homework.login.domain.Member;
+import homework.login.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setPassword(form.getPassword());
+
+        memberService.join(member);
+
+        return "redirect:/"; //회원가입이 끝나면 홈 화면으로 보냄
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
---
+++ seoyeon5117/hw3/src/main/java/homework/login/service/MemberService.java
@@ -0,0 +1,42 @@
+package homework.login.service;
+
+import homework.login.domain.Member;
+import homework.login.repository.MemberRepository;
+import homework.login.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //ctrl+Alt+M -> extract, 증복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
